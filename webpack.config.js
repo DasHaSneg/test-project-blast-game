c455b032156f28c0727fbdad516eda3e3c38327e
hide101100 <dashasneg@mail.ru>
2021-11-09 00:47:50 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.ts',
+	devtool: 'inline-source-map',
+	devServer: {
+		static: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		minimize: false,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+	],
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.ts',
+		path: path.resolve(__dirname, 'dist'),
+	},
+};
